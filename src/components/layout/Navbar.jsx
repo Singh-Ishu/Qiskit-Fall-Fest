78254ef762c69321f5b701e88fd9e4e5080dfd87
Singh-Ishu <itzshaansingh@gmail.com>
2025-09-21 23:27:42 +0530

    Added Navbar

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,27 @@
+import GooeyNav from "./GooeyNav";
+
+// update with your own items
+const items = [
+    { label: "Home", href: "#" },
+    { label: "About", href: "#" },
+    { label: "Contact", href: "#" },
+];
+
+function Navbar() {
+    return (
+        <div className="Navbar">
+            <GooeyNav
+                items={items}
+                particleCount={15}
+                particleDistances={[90, 10]}
+                particleR={100}
+                initialActiveIndex={0}
+                animationTime={600}
+                timeVariance={300}
+                colors={[1, 2, 3, 1, 2, 3, 1, 4]}
+            />
+        </div>
+    );
+}
+
+export default Navbar;
